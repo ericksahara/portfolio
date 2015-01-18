4d0c511f4e7ab91ac82c48ea21f84bf9296be60f
Erick Sahara <erickmsk8@hotmail.com>
2015-01-18 13:45:39 -0200

    fix config.rb

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/portfolio"
+http_path = "/"
 css_dir = "assets/css"
 sass_dir = "assets/sass"
 images_dir = "img"
