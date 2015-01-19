f096ccbdabba09c9003cf17555eaf697467d599f
Erick Sahara <erickmsk8@hotmail.com>
2015-01-19 20:02:17 -0200

    add icon-facebook.png

--- index.php
+++ index.php
@@ -11,10 +11,11 @@
     <meta property="og:description" content="Bem vindos ao meu portfólio! Tem um novo projeto? Vamos trocar ideias!"/> <!--max 200 char -->
     <meta property="og:type" content="website">
     <meta property="og:site_name" content="Erick Sahara - Portfólio">
-    <meta property="og:image" content="http://ericksahara.com/img/icon-iphone-retina.png">
+    <meta property="og:url" content="http://ericksahara.com">
+    <meta property="og:image" content="http://ericksahara.com/img/icon-facebook.png">
     <meta property="og:image:type" content="image/png">
-    <meta property="og:image:width" content="120">
-    <meta property="og:image:height" content="120">
+    <meta property="og:image:width" content="200">
+    <meta property="og:image:height" content="200">
     <link rel="dns-prefetch" href="//fonts.googleapis.com">
     <link rel="dns-prefetch" href="//maxcdn.bootstrapcdn.com">
     <link rel="dns-prefetch" href="//google-analytics.com">
